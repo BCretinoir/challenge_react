5f0ae35abce50caccd1e91f2648af2c4c29b6725
Bcretinoir <benjamin.cretinoir@hotmail.fr>
2026-02-13 14:46:57 +0100

    ajout des icons dans la barre de nav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function RootLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
-      <Tabs.Screen name="map" options={{ headerShown: false }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen name="map" options={{ headerShown: false, tabBarIcon: ({ color}) => <FontAwesome size={28} name="map-marker" color={color} /> }}  />
     </Tabs>
   );
 }
