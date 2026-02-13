08d4b0084b88a6a0f1b8ca657d13d0b94796dd34
Bcretinoir <benjamin.cretinoir@hotmail.fr>
2026-02-13 14:30:09 +0100

    j'suis un trouduc

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import {View} from "../../design-system/components/ui/View";
+
+export default function Home() {
+    return (
+        <View flex={1} center>
+            <h1>Home</h1>
+        </View>
+    );
+}
